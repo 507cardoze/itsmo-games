23067d3833d7bdfb27e86eb2d79250094d0bec13
Anthony Salvattore Cardoze Agrio <anthonycardoze@gmail.com>
2022-03-29 10:36:13 -0500

    subida inicial

--- common/checkAdmin.ts
+++
@@ -1,7 +0,0 @@
-const checkAdmin = (path: string, isAdmin: boolean | null | undefined) => {
-  const adminPath = ['/admin-panel'];
-  if (adminPath.includes(path) && !isAdmin) return true;
-  return false;
-};
-
-export default checkAdmin;
